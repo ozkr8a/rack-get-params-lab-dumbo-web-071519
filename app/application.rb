2bdb90a19232669cf5c77bdee0b25399cfa464ab
Oscar <oscar_ochoac@yahoo.com>
2019-08-05 13:57:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,7 @@ class Application
       resp.write "Path Not Found\n"
     elsif @@cart == []
       resp.write "Your cart is empty"
-    elsif req.path.match(/cart/)
+      if req.path.match(/cart/)
       @@cart.each do |item|
       resp.write "#{item}\n"
       end
